e84f664dc26c3a381df6c1935321c8e2d70ebcd9
avaj-12 <ashwing123987@gmail.com>
2026-02-17 20:20:39 +0530

    (setup): Basic Setup Done

---
+++ hotel-booking-app/src/app/App.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/layouts/Header";
+import Home  from "./home/Home";
+import Footer from "@/components/layouts/Footer";
+
+const App = () => {
+    return(
+        <>
+            <h1>App</h1>
+            <Header></Header>
+            <Home></Home>
+            <Footer></Footer>
+        </>
+    )
+}
+
+export default App;
--- hotel-booking-app/src/main.tsx
+++ hotel-booking-app/src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode, useState } from "react"
 import { createRoot } from "react-dom/client"
-import React from "react"
 import "./index.css"
 
 import { Calendar } from "@/components/ui/calendar"
